e6098d0e5a71800a77ce20b2137569c3a4e9d69c
Арсений Падерин <arseniypaderin@gmail.com>
2024-02-15 01:10:57 +0600

    Add comment

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
+# Тест коммент
 
 urlpatterns = [
     path('admin/', admin.site.urls),
